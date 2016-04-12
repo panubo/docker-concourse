1b834076c314da9319f7e656575d02bb216b3ccb
Andrew Cutler <andrew@panubo.com>
2016-04-12 13:49:46 +1000

    add --postgres-data-source from linked container

--- entry.sh
+++ entry.sh
@@ -6,8 +6,15 @@ set -e
 
 echo "Running $@"
 if [ "$(basename $1)" == "concourse" ]; then
-    #
-    exec concourse
+
+    if [ "$2" == "web" ]; then
+        PG_CONN="postgres://${POSTGRES_ENV_POSTGRES_USER}:${POSTGRES_ENV_POSTGRES_PASSWORD}@${POSTGRES_PORT_5432_TCP_ADDR}:${POSTGRES_PORT_5432_TCP_PORT}/${POSTGRES_ENV_POSTGRES_USER}?sslmode=disable"
+        exec "$@" --postgres-data-source="$PG_CONN"
+    else
+        # worker or other
+        # NB. Currently worker doesn't function in docker.
+        exec "$@"
+    fi
 else
     exec "$@"
 fi
